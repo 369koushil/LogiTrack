173cbff9cbfc18529eedfe0700ab45615e855d82
koushil <154089176+369koushil@users.noreply.github.com>
2024-11-24 15:15:00 +0530

    Update AddProduct.jsx

--- Frontend/src/components/AddProduct.jsx
+++ Frontend/src/components/AddProduct.jsx
@@ -23,7 +23,7 @@ export default function AddProduct({
   };
 
   const addProduct = () => {
-    fetch("http://localhost:4000/api/product/add", {
+    fetch(`${process.env.REACT_APP_API_END_POINT}/product/add', {
       method: "POST",
       headers: {
         "Content-type": "application/json",
